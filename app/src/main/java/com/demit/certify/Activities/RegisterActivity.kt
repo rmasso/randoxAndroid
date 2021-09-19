06435c4237ed89b741aedb88cb39c38ccd0ef9d9
hammad1994 <hammad7294@gmail.com>
2021-09-20 00:28:36 +0500

    Blink id sdk integrated in register activity. Now user can use passport scan feature.

--- app/src/main/java/com/demit/certify/Activities/RegisterActivity.kt
+++ app/src/main/java/com/demit/certify/Activities/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.demit.certify.Activities
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,26 +14,21 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.demit.certify.Extras.Constants
 import com.demit.certify.Extras.Functions
-import com.demit.certify.Extras.Shared
 import com.demit.certify.Extras.Sweet
-import com.demit.certify.Models.TProfileModel
-import com.demit.certify.R
 import com.demit.certify.databinding.ActivityProfileBinding
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-//import com.microblink.entities.recognizers.Recognizer
-//import com.microblink.entities.recognizers.RecognizerBundle
-//import com.microblink.entities.recognizers.blinkid.generic.BlinkIdCombinedRecognizer
-//import com.microblink.uisettings.ActivityRunner
-//import com.microblink.uisettings.BlinkIdUISettings
+import com.microblink.entities.recognizers.Recognizer
+import com.microblink.entities.recognizers.RecognizerBundle
+import com.microblink.entities.recognizers.blinkid.generic.BlinkIdCombinedRecognizer
+import com.microblink.uisettings.ActivityRunner
+import com.microblink.uisettings.BlinkIdUISettings
 import kotlinx.android.synthetic.main.activity_profile.*
 import org.json.JSONObject
 import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
     //Blink Id Variables
-//    private lateinit var mRecognizer: BlinkIdCombinedRecognizer
-//    private lateinit var mRecognizerBundle: RecognizerBundle
+    private lateinit var mRecognizer: BlinkIdCombinedRecognizer
+   private lateinit var mRecognizerBundle: RecognizerBundle
     private val MY_REQUEST_CODE = 801
     lateinit var sweet : Sweet
     lateinit var binding : ActivityProfileBinding
@@ -41,13 +37,10 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
         sweet = Sweet(this)
-
-//        mRecognizer = BlinkIdCombinedRecognizer()
-//        mRecognizer.setReturnFaceImage(true)
-        // bundle recognizers into RecognizerBundle
-//        mRecognizerBundle = RecognizerBundle(mRecognizer)
-//        startScanning()
         clicks()
+        mRecognizer = BlinkIdCombinedRecognizer()
+        mRecognizer.setReturnFaceImage(true)
+        mRecognizerBundle = RecognizerBundle(mRecognizer)
     }
     private fun clicks() {
         Log.d("ssss" , "here")
@@ -68,65 +61,56 @@ class RegisterActivity : AppCompatActivity() {
                 register()
             }
         }
+
+        binding.scanYourPassport.setOnClickListener {
+                startScanning()
+        }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (requestCode === MY_REQUEST_CODE) {
-//            if (resultCode === RESULT_OK && data != null) {
-//                // load the data into all recognizers bundled within your RecognizerBundle
-//                mRecognizerBundle.loadFromIntent(data)
-//
-//                // now every recognizer object that was bundled within RecognizerBundle
-//                // has been updated with results obtained during scanning session
-//                // you can get the result by invoking getResult on recognizer
-//                val result = mRecognizer.result
-//                // result is valid, you can use it however you wish
-//                if (result.resultState == Recognizer.Result.State.Valid) {
-//                    fname.text= Editable.Factory.getInstance().newEditable( result.firstName)
-//                    sname.text= Editable.Factory.getInstance().newEditable( result.lastName)
-//
-//
-//                    result.dateOfBirth.date?.let {date->
-//                        val day= date.day
-//                        val year= date.year
-//                        val month= date.month
-//                        dob.text= Editable.Factory.getInstance().newEditable("$day/$month/$year")
-//                    }
-//
-//
-//                    pnumber.text= Editable.Factory.getInstance().newEditable( result.documentNumber)
-//
-//
-//
-//                    /* val imageArr = result.encodedFaceImage
-//                     faceImage.setImageBitmap(
-//                         result.faceImage?.convertToBitmap()
-//                     )*/
-//
-//                    /*     val p = ProfileModel();
-//                     list.add(p)
-//                     binding.rv.getAdapter()?.itemCount?.minus(1)?.let { it1 ->
-//                         binding.rv.smoothScrollToPosition(
-//                             it1
-//                         )
-//                     };
-//                     setData(list.size - 1)*/
-//
-//                }
-//            }
-//        }
-//    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK && data != null) {
+                // load the data into all recognizers bundled within your RecognizerBundle
+                mRecognizerBundle.loadFromIntent(data)
 
-//    private fun startScanning() {
-//        // Settings for BlinkIdActivity
-//        val settings = BlinkIdUISettings(mRecognizerBundle)
-//
-//        // tweak settings as you wish
-//
-//        // Start activity
-//        ActivityRunner.startActivityForResult(this, MY_REQUEST_CODE, settings)
-//    }
+                // now every recognizer object that was bundled within RecognizerBundle
+                // has been updated with results obtained during scanning session
+                // you can get the result by invoking getResult on recognizer
+                val result = mRecognizer.result
+                // result is valid, you can use it however you wish
+                if (result.resultState == Recognizer.Result.State.Valid) {
+                    fname.text= Editable.Factory.getInstance().newEditable( result.firstName)
+                    sname.text= Editable.Factory.getInstance().newEditable( result.lastName)
+
+
+                    result.dateOfBirth.date?.let {date->
+                        val day= date.day
+                        val year= date.year
+                        val month= date.month
+                        dob.text= Editable.Factory.getInstance().newEditable("$day/$month/$year")
+                    }
+
+
+                    pnumber.text= Editable.Factory.getInstance().newEditable( result.documentNumber)
+
+
+
+
+                }
+            }
+        }
+    }
+
+    private fun startScanning() {
+        // Settings for BlinkIdActivity
+        val settings = BlinkIdUISettings(mRecognizerBundle)
+
+        // tweak settings as you wish
+
+        // Start activity
+        ActivityRunner.startActivityForResult(this, MY_REQUEST_CODE, settings)
+    }
 
     fun register(){
         sweet.show("Registering User")
--- app/src/main/java/com/demit/certify/BaseApp.kt
+++ app/src/main/java/com/demit/certify/BaseApp.kt
@@ -2,23 +2,17 @@ package com.demit.certify
 
 import android.app.Application
 import android.graphics.Bitmap
-//import com.microblink.*
-//import com.microblink.intent.IntentDataTransferMode
-//import com.microblink.licence.exception.InvalidLicenceKeyException
-//import com.zynksoftware.documentscanner.ui.DocumentScanner
-
+import com.microblink.MicroblinkSDK
+import com.microblink.intent.IntentDataTransferMode
+import com.microblink.licence.exception.InvalidLicenceKeyException
 class BaseApp: Application() {
     override fun onCreate() {
         super.onCreate()
-//        val configuration = DocumentScanner.Configuration()
-//        configuration.imageQuality = 100
-//        configuration.imageType = Bitmap.CompressFormat.JPEG
-//        DocumentScanner.init(this, configuration) //
-//        try {
-//            MicroblinkSDK.setLicenseFile("license.key",this)
-//            MicroblinkSDK.setIntentDataTransferMode(IntentDataTransferMode.PERSISTED_OPTIMISED)
-//        }catch(ex: InvalidLicenceKeyException){
-//            ex.printStackTrace()
-//        }
+        try {
+            MicroblinkSDK.setLicenseFile("license.key",this)
+            MicroblinkSDK.setIntentDataTransferMode(IntentDataTransferMode.PERSISTED_OPTIMISED)
+        }catch(ex: InvalidLicenceKeyException){
+            ex.printStackTrace()
+        }
     }
 }
