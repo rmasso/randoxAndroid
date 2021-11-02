1228571e4fcfb1e8d1a83945ee2cbc2c79524dcf
hammad1994 <hammad7294@gmail.com>
2021-11-02 22:26:34 +0500

    New youtube link added for faq.

--- app/src/main/java/com/demit/certifly/Extras/Constants.kt
+++ app/src/main/java/com/demit/certifly/Extras/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
         const val url= "https://certiflyapi.massolabs.com/certifly/"
         const val adminToken = "fcy_9fb620fd5a2f9ded119397db3a6d164a";
         const val DEFAULT_APP_FOLDER="Randox"
-        const val VIDEO_ID_HOW_IT_WORKS="6EjpnbBbgXM"
+        const val VIDEO_ID_HOW_IT_WORKS="3y5m-ByKMZ8"
 
     }
 }
