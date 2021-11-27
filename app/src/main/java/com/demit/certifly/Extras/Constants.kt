7a7f64e17709b48919521d716ab49163241a5277
hammad1994 <hammad7294@gmail.com>
2021-11-28 04:13:04 +0500

    New endpoint url added.

--- app/src/main/java/com/demit/certifly/Extras/Constants.kt
+++ app/src/main/java/com/demit/certifly/Extras/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object{
        // const val url = "http://www.massolabs.com/certifly/";
-        const val url= "https://certiflyapi.massolabs.com/certifly/"
+        const val url= "https://api.certifly.co/certifly/"
         const val adminToken = "fcy_9fb620fd5a2f9ded119397db3a6d164a";
         const val DEFAULT_APP_FOLDER="Randox"
         const val VIDEO_ID_HOW_IT_WORKS="3y5m-ByKMZ8"
