b81074352adb746e17b400c52e0eebbe00eb554c
hammad1994 <hammad7294@gmail.com>
2022-01-24 00:55:22 +0500

    New Faq layout design.

--- app/src/main/java/com/demit/certifly/Fragments/FaqFragment.kt
+++ app/src/main/java/com/demit/certifly/Fragments/FaqFragment.kt
@@ -5,23 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.demit.certifly.Interfaces.FaqScrollInterface
-import com.demit.certifly.Models.FaqAsk
-import com.demit.certifly.adapters.FaqAdapter
 import com.demit.certifly.databinding.FragmentFaqBinding
-import android.content.ActivityNotFoundException
 
-import android.content.Intent
-import android.net.Uri
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
-import com.demit.certifly.Extras.Constants
+import android.text.method.LinkMovementMethod
+import androidx.appcompat.app.AppCompatActivity
 import com.demit.certifly.R
+import com.demit.certifly.adapters.VideoSliderAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 
-class FaqFragment : Fragment(), FaqScrollInterface {
+class FaqFragment : Fragment() {
     lateinit var binding: FragmentFaqBinding
-    lateinit var adapter: FaqAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,131 +27,19 @@ class FaqFragment : Fragment(), FaqScrollInterface {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initAdapter()
-        initRecyclerView()
-        Glide.with(requireContext())
-            .load(R.drawable.thumbnail3)
-            .placeholder(R.drawable.thumbnail3)
-            .into(binding.youtubePlayer)
-
-        binding.youtubePlayer.setOnClickListener {
-            launchYoutube()
-        }
-    }
-
-    private fun initRecyclerView() {
-        with(binding) {
-            this.rvFaq.layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-            this.rvFaq.isNestedScrollingEnabled = false
-            this.rvFaq.adapter = adapter
-
-        }
+        setUpSlider()
+        binding.faqText.movementMethod = LinkMovementMethod()
 
     }
 
-    private fun initAdapter() {
-        adapter = FaqAdapter(getSampleList(), this)
+    private fun setUpSlider() {
+        val totalPages = resources.getStringArray(R.array.faq_video_ids).size
+        binding.slider.adapter =
+            VideoSliderAdapter(requireActivity() as AppCompatActivity, totalPages)
+
+        TabLayoutMediator(binding.pageIndicator, binding.slider)
+        { _, _ ->}.attach()
     }
 
-    private fun getSampleList(): List<FaqAsk> {
-        val faqAskList = ArrayList<FaqAsk>()
-
-
-        faqAskList.add(
-            FaqAsk(
-                question = "How do I use the COVID-19 sample collection kit?",
-                answer = "Each sample collection kit contains instructions for use. It is the user’s responsibility to ensure the instructions are read in full prior to engaging in the sampling process."
-            )
-        )
-        faqAskList.add(
-            FaqAsk(
-                question = "Will I get a travel certificate?",
-                answer = "You will receive your results via email, as well as a certificate of your results to use whilst travelling. In order for your certificate to be correct, please ensure the information used during the registration process matches the details on your passport"
-            )
-        )
-        faqAskList.add(
-            FaqAsk(
-                question = "Do I need to prepare for my sample?",
-                answer = "Food or drink should not be consumed within the 30 minutes"
-            )
-        )
-
-        faqAskList.add(
-            FaqAsk(
-                question = "What does it mean if I have a negative result?",
-                answer = "A negative test results means that you are unlikely to have COVID-19."
-            )
-        )
-        faqAskList.add(
-            FaqAsk(
-                question = "What is the difference between an Antigen test and PCR test?",
-                answer = "PCR (polymerase chain reaction) tests have been designed to find genetic material called RNA that tells the virus to make these proteins. This test the most accurate Covid-19 test. When swabs are taken for PCR tests, reagents are used to convert the RNA into DNA in order to identify any infection within the patient. Antigen tests have been designed to find proteins on the surface of the virus to identify the pathogen. Antigen tests are often called rapid tests and are carried out by mixing the sample with a solution where it releases the viral proteins. The combination is normally applied on a paper strip that will be able to detect any antibodies that bind the protein."
-            )
-        )
-        faqAskList.add(
-            FaqAsk(
-                question = "How accurate are the antigen tests?",
-                answer = "The test meets performance standards of ≥97% specificity, ≥80% sensitivity at viral loads above 100,000 copies/ml"
-            )
-        )
-        faqAskList.add(
-            FaqAsk(
-                question = "Can I reuse a test?",
-                answer = "No. Each test is single use only."
-            )
-        )
-
-        faqAskList.add(
-            FaqAsk(
-                question = "Are your kits on the EUR approval list?",
-                answer = "Yes."
-            )
-        )
-
-        faqAskList.add(
-            FaqAsk(
-                question = "What is the purchase order?",
-                answer = "This is the unique order number that you received on the order confirmation that you received from Randox when the order was placed- on the order confirmation this begins with # followed by a series of numbers. When entering in this number into the APP do not include the #"
-
-            )
-        )
-
-        faqAskList.add(
-            FaqAsk(
-                question = "What is the Day 2 Booking Reference?",
-                answer = "This is the specific Unique booking reference number that you would have used to complete the passenger locator form, you will find this within your original order confirmation and it begins with RANDX followed by a series of numbers. For each day 2 Antigen Lateral Flow ordered, there will be a specific unique booking reference number."
-
-            )
-        )
-
-        faqAskList.add(
-            FaqAsk(
-                question = "Where can I find more info?",
-                answer = "Randox.com"
-            )
-        )
-
-
-        return faqAskList
-    }
-
-    override fun scrollTo(position: Int) {
-        binding.rvFaq.smoothScrollToPosition(position)
-    }
-
-    private fun launchYoutube() {
-        val appIntent =
-            Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:${Constants.VIDEO_ID_HOW_IT_WORKS}"))
-        val webIntent = Intent(
-            Intent.ACTION_VIEW,
-            Uri.parse("http://www.youtube.com/watch?v=${Constants.VIDEO_ID_HOW_IT_WORKS}")
-        )
-        try {
-            requireContext().startActivity(appIntent)
-        } catch (ex: ActivityNotFoundException) {
-            requireContext().startActivity(webIntent)
-        }
-    }
 
 }
